0169f1c77bfbc98a6f21e22f1637d291b8527aa5
worldmaker18349276 <worldmaker18349276@gmail.com>
2023-05-04 09:38:10 +0800

    add Graphics

---
+++ HyperRubiksCube/Graphics.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Graphics;
+
+namespace Graphics;
+
+public class GraphicsDrawable : IDrawable
+{
+    void IDrawable.Draw(ICanvas canvas, RectF dirtyRect)
+    {
+        canvas.StrokeColor = Colors.Red;
+        canvas.StrokeSize = 1;
+        canvas.DrawLine(10, 10, 90, 100);
+    }
+}
+
