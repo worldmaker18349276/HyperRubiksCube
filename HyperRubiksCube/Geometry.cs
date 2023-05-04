2d8ceb61143765bbb893876a9b1caa0b2c380a24
worldmaker18349276 <worldmaker18349276@gmail.com>
2023-05-04 11:36:00 +0800

    add Geomtry

---
+++ HyperRubiksCube/Geometry.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using System.Numerics;
+
+namespace Geometry;
+
+class Face3
+{
+    public Vector3 Normal { get; set; }
+    public List<Vector3> Vertices { get; set; }
+
+    public Face3(Vector3 normal, List<Vector3> vertices)
+    {
+        Normal = normal;
+        Vertices = vertices;
+    }
+
+    public void Transform(Quaternion rotation)
+    {
+        Normal = Vector3.Transform(Normal, rotation);
+        Vertices = Vertices.Select(v => Vector3.Transform(v, rotation)).ToList();
+    }
+}
+
+class Face2
+{
+    public List<Vector2> Vertices { get; set; }
+
+    public Face2(List<Vector2> vertices)
+    {
+        Vertices = vertices;
+    }
+}
+
+class Camera3
+{
+    public Quaternion Orientation { get; set; }
+    public float FocalLength { get; set; }
+    public float ScreenDistance { get; set; }
+
+    public Vector3 Forward
+    {
+        get
+        {
+            return Vector3.Transform(new Vector3(0, 0, -1), Orientation);
+        }
+    }
+
+    public Vector3 Upward
+    {
+        get
+        {
+            return Vector3.Transform(new Vector3(0, 1, 0), Orientation);
+        }
+    }
+
+    public Camera3(Quaternion orientation, float focalLength, float screenDistance)
+    {
+        Orientation = orientation;
+        FocalLength = focalLength;
+        ScreenDistance = screenDistance;
+    }
+
+    public void Transform(Quaternion rotation)
+    {
+        Orientation = rotation * Orientation;
+    }
+
+    public Vector2 ProjectVector(Vector3 position)
+    {
+        position = Vector3.Transform(position, Orientation);
+
+        if (float.IsInfinity(FocalLength))
+            return new Vector2(position.X, position.Y);
+
+        var scale = (FocalLength - ScreenDistance) / (FocalLength - position.X);
+        return new Vector2(position.X * scale, position.Y * scale);
+    }
+
+    public Face2 ProjectFace(Face3 face)
+    {
+        if (Vector3.Dot(face.Normal, Forward) > 0)
+            return null;
+
+        var vertices = face.Vertices.Select(this.ProjectVector).ToList();
+        return new Face2(vertices);
+    }
+}
+
+class Coordinate
+{
+    public PointF Center { get; set; }
+    public float Ratio { get; set; }
+
+    public Coordinate(PointF center, float ratio)
+    {
+        Center = center;
+        Ratio = ratio;
+    }
+
+    public PointF Convert(Vector2 point)
+    {
+        return new PointF(Center.X + point.X * Ratio, Center.Y + point.Y * Ratio);
+    }
+}
--- HyperRubiksCube/Graphics.cs
+++ HyperRubiksCube/Graphics.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Maui.Graphics;
+﻿using Geometry;
+using System.Numerics;
 
 namespace Graphics;
 
@@ -6,9 +7,45 @@ public class GraphicsDrawable : IDrawable
 {
     void IDrawable.Draw(ICanvas canvas, RectF dirtyRect)
     {
-        canvas.StrokeColor = Colors.Red;
-        canvas.StrokeSize = 1;
-        canvas.DrawLine(10, 10, 90, 100);
+        canvas.FontColor = Colors.Blue;
+        canvas.FontSize = 18;
+        canvas.Font = Microsoft.Maui.Graphics.Font.Default;
+        canvas.FillColor = Colors.DarkGray;
+        canvas.FillRectangle(dirtyRect);
+
+        var coordinate = new Coordinate(
+            center: dirtyRect.Center,
+            ratio: 50
+        );
+
+        var camera = new Camera3(
+            orientation: Quaternion.Identity,
+            focalLength: float.PositiveInfinity,
+            screenDistance: 2
+        );
+
+        var face1 = new Face3(
+            new Vector3(0, 0, 1),
+            new List<Vector3> {
+                new( 1,  1, 0),
+                new(-1,  1, 0),
+                new(-1, -1, 0),
+                new( 1, -1, 0)
+            }
+        );
+        face1.Transform(Quaternion.CreateFromAxisAngle(new(1, 1, 0), float.Pi/5));
+
+        var rect1 = camera.ProjectFace(face1);
+        if (rect1 != null)
+        {
+            var reg = new PathF();
+            if (rect1.Vertices.Any())
+                reg.MoveTo(coordinate.Convert(rect1.Vertices.First()));
+            foreach (var curr in rect1.Vertices.Skip(1))
+                reg.LineTo(coordinate.Convert(curr));
+            canvas.FillColor = Colors.SlateBlue;
+            canvas.FillPath(reg);
+        }
     }
 }
 
